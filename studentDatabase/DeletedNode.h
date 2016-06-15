1357ed7c702d27d72374aa40efa2ba4fc2b6b97f
michaelqvphan <michaelqvphan@gmail.com>
2016-06-15 01:26:57 -0700

    Update DeletedNode.h
    added comments

--- studentDatabase/DeletedNode.h
+++ studentDatabase/DeletedNode.h
@@ -4,20 +4,23 @@ template <class key_type, class value_type>
 class DeletedNode : public HashNode<key_type, value_type>
 {
 private:
-	static DeletedNode * entry;
-	DeletedNode() : HashNode() {}
+	static DeletedNode * entry; // creating an entry to be used as the deleted node
+	DeletedNode() : HashNode() {} // using the constructor from the hash node class
 
 public:
 	static DeletedNode * getUniqueDeletedNode();
 };
 
+// Function to create a new unique deleted node everytime the function is called
+// this is to make sure the deleted node is accounted for in the hash table when a node is deleted
 template <class key_type, class value_type>
 DeletedNode<key_type, value_type> * DeletedNode<key_type, value_type>::getUniqueDeletedNode()
 {
 	if (entry == NULL)
-		entry = new DeletedNode();
+		entry = new DeletedNode(); 
 	return entry;
 }
 
+// creation of a deleted node pointer to null 
 template <class key_type, class value_type>
-DeletedNode<key_type, value_type> * DeletedNode<key_type, value_type>::entry = NULL;
+DeletedNode<key_type, value_type> * DeletedNode<key_type, value_type>::entry = NULL; 
