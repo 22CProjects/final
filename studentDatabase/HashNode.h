0652969d1e8d20b298703af57340c902c772b652
michaelqvphan <michaelqvphan@gmail.com>
2016-06-15 20:57:35 -0700

    Update HashNode.h

--- studentDatabase/HashNode.h
+++ studentDatabase/HashNode.h
@@ -17,7 +17,7 @@ private:
 public:
 	HashNode() {} // default constructor
 	HashNode(key_type key, value_type value) { this->key = key; this->value = value; } // constructor with 2 constructors
-	int get_key() { return key; } // function to return the key
+	key_type get_key() { return key; } // function to return the key
 	value_type get_value() { return value; } //  function to return the value of the specific key
 	void set_value(value_type value) { this->value = value; } // function to set the value
 };
